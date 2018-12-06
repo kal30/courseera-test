1cb50840c06aec070cce8a14bbe02801f2c67d8c
rkalyani@gmail.com <krajamani@someemail.com>
2018-12-06 11:22:11 -0600

    First commit to shopping list

--- Lecture 09/Interpolation/app.js
+++ Lecture 09/Interpolation/app.js
@@ -3,19 +3,47 @@
     'use strict';
 
     angular.module('MsgApp', [])
-        .controller('MsgController', MsgController);
+        .controller('MsgController', MsgController)
+        .filter('loves', LovesFilter)
+        .filter ('truth', TruthFilter);
 
-    MsgController.$inject = ['$scope'] //this helps when we minify
-    function MsgController($scope) {
+    MsgController.$inject = ['$scope', 'lovesFilter'] //this helps when we minify
+    function MsgController($scope, lovesFilter) {
         $scope.name = "Kalyani";
         $scope.stateOfBeing = "flower";
+        $scope.cookieCost = .45;
+
         $scope.sayMessage = function () {
-            return "Some message";
+            var msg = "This has likes";
+            //var output = $filter('uppercase')(msg);
+            return msg;
+        };
+
+        $scope.sayLovesMessage = function () {
+            var msg = "This is likes healthy";
+            var output = lovesFilter(msg);
+            return output;
         };
 
         $scope.flipImage = function () {
             $scope.stateOfBeing = "mountain";
         };
+    }
+
+    function LovesFilter() {
+        return function (input) {
+            input = input || "";
+            input = input.replace("likes", "loves");
+            return input;
+        };
+    }
+
+    function TruthFilter() {
+        return function (input, target, replace){
+            input = input || "";
+            input = input.replace("target", "replace");
+            return input;
+        }
     }
 
 })();
